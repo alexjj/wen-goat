4b142acebfe08b29ff72186194f13c0e2ef24f54
OE5JFE <joefellner@gmx.at>
2025-02-12 16:33:58 +0100

    yticks only for multiples of 1000 points

--- app.py
+++ app.py
@@ -52,8 +52,12 @@ def plot_progress(data, avg_points_per_week, target_points):
     future_points = [last_total + avg_points_per_week_calc * i for i in range(len(future_dates))]
     plt.plot(future_dates, future_points, linestyle=':', color='orange', label='Projected Progress (linear)')
 
+    ylabels = []
     for milestone in range(1000, next_target + 1000, 1000):
         plt.axhline(milestone, color='grey', linestyle=':', alpha=0.5)
+        ylabels.append(milestone)
+    plt.yticks(ylabels) #  Only full MG labels on y-axis
+
     plt.axhline(target_points, color='green', linestyle=':', label=f'Next Milestone ({target_points} pts)')
     plt.vlines(x= datetime.now(),ymin=0,ymax=target_points, color='grey', linestyle=':', alpha=0.25) # Mark the nowdate
     plt.xlabel('Date')
